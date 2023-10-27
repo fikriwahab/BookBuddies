93ab7fdfacd6541216ddc1761bd1daf927fecb8a
fikriwahab <fikrimassaidwahab2@gmail.com>
2023-10-27 14:40:26 +0700

    Tampilan Katalog dengan Dataset (tanpa Cover)

---
+++ katalog/views.py
@@ -0,0 +1,6 @@
+from django.shortcuts import render
+from .models import Book
+
+def book_list(request):
+    books = Book.objects.all()
+    return render(request, 'book_list.html', {'books': books})
