06a181e130132db1595a8f0335741c56e80bf63c
Limar Suci Rahayu <limar.rahayu@pertamina.com>
2023-10-28 23:27:52 +0700

    Dasboard-member

--- BookBuddies/settings.py
+++ BookBuddies/settings.py
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'HalamanInformasiBuku',
     'katalog',
+    'dashboard_member'
 ]
 
 MIDDLEWARE = [
---
+++ dashboard_member/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path
+from . import views
+
+app_name = 'HalamanInformasiBuku'
+
+urlpatterns = [
+    path('book/<int:book_id>/', views.book_detail, name='book_detail'),
+    path('book/<int:book_id>/loan/', views.ajax_create_loan, name='ajax_create_loan'),
+    path('book/<int:book_id>/read/', views.ajax_read_book, name='ajax_read_book'),
+    path('dashboard/', views.member_dashboard, name='member_dashboard'),  
+]
