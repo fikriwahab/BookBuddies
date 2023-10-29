4169a87f87b9c304c75d81e8ea557a2248bbc6a3
Marchelina Anjani S <marselinaanjanisaputri23@gmail.com>
2023-10-29 18:16:03 +0700

    update style katalog

--- katalog/urls.py
+++ katalog/urls.py
@@ -1,5 +1,7 @@
 from django.urls import path
 from . import views
+from BookBuddies import settings
+from django.conf.urls.static import static
 
 app_name = "katalog"
 
@@ -9,3 +11,6 @@ urlpatterns = [
     path('login/', views.user_login, name='login'),
     path('logout/', views.user_logout, name='user_logout'),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
