925ebe7228b80ed69dbb152bb0105f66bfdb49e4
syarna <syarnasavitri66@gmail.com>
2023-10-29 21:00:46 +0700

    .

--- BookBuddies/settings.py
+++ BookBuddies/settings.py
@@ -44,11 +44,8 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'HalamanInformasiBuku',
     'katalog',
-<<<<<<< HEAD
     'DashboardAdmin',
     'bookmarks',
-=======
->>>>>>> origin/dev
 ]
 
 MIDDLEWARE = [
