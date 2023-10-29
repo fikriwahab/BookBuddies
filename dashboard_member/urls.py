73e4836b26cb4af26a77ca63052cefa25d6ef407
Limar Suci Rahayu <limar.rahayu@pertamina.com>
2023-10-29 21:18:09 +0700

    Update new

--- dashboard_member/urls.py
+++ dashboard_member/urls.py
@@ -1,8 +1,15 @@
 from django.urls import path
-from . import views
+from dashboard_member.views import *
 
 app_name = 'dashboard_member'
 
 urlpatterns = [
-    path('', views.member_dashboard, name='member_dashboard'),
+    path('', index, name='dashboard'),
+    path('logout', sign_out, name='logout'),
+    path('profile', profile, name='profile'),
+    path('bookmarks', bookmarks, name='bookmarks'),
+    path('recommended', recommended, name='recommended'),
+    path('create-book', create_book, name='create_book'),
+    path('manage_book', manage_book, name='manage_book'),
+    path('delete_book/<int:book_id>', delete_book, name='delete_book'),
 ]
