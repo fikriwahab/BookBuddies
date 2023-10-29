687c6b9db792a6c0399f70c6a109f7b4118588df
syarna <syarnasavitri66@gmail.com>
2023-10-29 20:26:14 +0700

    benerin halaman info

--- HalamanInformasiBuku/urls.py
+++ HalamanInformasiBuku/urls.py
@@ -10,13 +10,8 @@ urlpatterns = [
     path('book/<int:book_id>/', book_detail, name='book_detail'),
     path('tambah_peminjam/<int:book_id>/', tambah_peminjam, name='tambah_peminjam'),
     path('get-product-json/', get_product_json, name='get_product_json'),
-<<<<<<< HEAD
     path('get-json/', get_json, name='get_json'),
-=======
     path('get_json/<int:book_id>/', get_json, name='get_json'),
-
-
->>>>>>> origin/dev
 ]
 
 
