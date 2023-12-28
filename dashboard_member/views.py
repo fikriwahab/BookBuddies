c92d26a3a115eeb5c027f082c83e38947e18dd73
Limar Suci Rahayu <limar.rahayu@pertamina.com>
2023-12-28 08:09:42 +0700

    Your commit message here

--- dashboard_member/views.py
+++ dashboard_member/views.py
@@ -3,12 +3,32 @@ from django.contrib.auth.decorators import login_required
 from django.contrib.auth import logout
 from django.http import JsonResponse
 from katalog.models import Book
-from .forms import BookForm
+from rest_framework.decorators import api_view, permission_classes
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.response import Response
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.models import User
+from django.http import JsonResponse
+from .serializers import UserSerializer
 
-# Create your views here.
+@api_view(['GET', 'PUT'])
+@permission_classes([IsAuthenticated])
+def edit_profile(request):
+    user = request.user
 
-@login_required
-def index(request):
+    if request.method == 'GET':
+        serializer = UserSerializer(user)
+        return Response(serializer.data)
+
+    elif request.method == 'PUT':
+        serializer = UserSerializer(user, data=request.data, partial=True)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=400)
+
+
+def dashboard (request):
     book_count = Book.objects.count()
 
     context = {
@@ -18,20 +38,16 @@ def index(request):
 
     return render(request, 'index.html', context)
 
-@login_required
+
 def profile(request):
     return render(request, 'profile.html', {'user': request.user, 'page_title': 'Profile'})
 
-@login_required
 def bookmarks(request):
     return render(request, 'bookmarks.html', {'page_title': 'Bookmarks'})
 
-@login_required
 def recommended(request):
     return render(request, 'recommended.html', {'page_title': 'Recommended'})
 
-@login_required
 def sign_out(request):
     logout(request)
     return redirect('/')
-
